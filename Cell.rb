0eaf1c9aae7a3a573895b06eb245dee6b6b2f474
Chris Cotton <chris.cottonn@gmail.com>
2015-03-26 12:43:32 -0700

    Cleaned up style for cell class

--- Cell.rb
+++ Cell.rb
@@ -1,6 +1,7 @@
+# Represents a cell in a Tic-Tac-Toe grid.
 class Cell
-	attr_accessor :value
-	def initialize(value = "")
-		@value = value	
-	end
+  attr_accessor :value
+  def initialize(value = '')
+    @value = value
+  end
 end
